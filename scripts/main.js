d394213840bb876f9a4252b62c8c31d2cb474672
Akufoolz <akufoolz@gmail.com>
2017-11-11 09:18:41 -0700

    added resize functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,39 @@
 // main.js
 
+// ++++++++++++++++++
+// Define Functions +
+// ++++++++++++++++++
+
 // create a function to quickly select elements
 function $e (elem) {
 	return document.querySelector(elem);
 }
 
-// +++++++++++++++
-// Set Variables +
-// +++++++++++++++
-
-// get client height and width
-var windowHeight = document.documentElement.clientHeight;
-var windowWidth = document.documentElement.clientWidth;
-
-// set divs into variables
-var tableDiv = $e(".table"); 
-var topDiv = $e(".top"); 
-var tableHeaderDiv = $e(".table-header"); 
-var bottomDiv = $e(".bottom");
-
-// get the padding of the div.table
-var tablePadding = (parseInt(getComputedStyle(tableDiv).paddingTop, 10) * 2);
-
-// add top two div heights together
-var topHeight = topDiv.clientHeight + tableHeaderDiv.clientHeight;
-
-// ++++++++++++++++++
-// Define Functions +
-// ++++++++++++++++++
-
 // sets height and width of the window
 function getWindow() {
-	windowHeight = document.documentElement.clientHeight;
-	windowWidth = document.documentElement.clientWidth;
+	// get client height and width
+	var windowHeight = document.documentElement.clientHeight;
+	var windowWidth = document.documentElement.clientWidth;
+	return {height: windowHeight, width: windowWidth};
 }
 
 // set height to remaining space by subtracting the height of the top divs and bottom div from window height
 function setTable() {
-	tableDiv.style.height =  windowHeight - (topHeight + bottomDiv.clientHeight + tablePadding) + "px"; 
-	tableDiv.style.width =  windowWidth - tablePadding + "px"; 
+	// set divs into variables
+	var tableDiv = $e(".table"); 
+	var topDiv = $e(".top"); 
+	var tableHeaderDiv = $e(".table-header"); 
+	var bottomDiv = $e(".bottom");
+
+	// get the padding of the div.table
+	//var tablePadding = (parseInt(getComputedStyle(tableDiv).paddingTop, 10)) + (parseInt(getComputedStyle(tableDiv).paddingBottom, 10));
+	var tablePadding = 32;
+	// add top two div heights together
+	var topHeight = topDiv.clientHeight + tableHeaderDiv.clientHeight;
+	
+	// set div.table width and height
+	tableDiv.style.height =  getWindow().height - (topHeight + bottomDiv.clientHeight + tablePadding) + "px"; 
+	tableDiv.style.width =  getWindow().width - tablePadding + "px"; 
 }
 
 // resizes div.table when the browser is resized
@@ -47,14 +42,18 @@ function resizeTable() {
 	setTable();
 	console.log("resized!");
 }
-/*
-// trigger getWindow function on load
-document.addEventListener('DOMContentLoaded', function() {
-    getWindow;
-}, false);
-*/
 
-getWindow();
-setTable();
+// main program loop
+
+function main() {
+	getWindow();
+	setTable();
+	console.log(getWindow().height + " x " + getWindow().width);
+}
+
+// ++++++++++++++++++
+// Define Functions +
+// ++++++++++++++++++
+
+main();
 window.onresize = resizeTable; 
-console.log(windowHeight + " x " + windowWidth);
